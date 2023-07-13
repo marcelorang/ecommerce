d541b393e4a52b9716ef5c6b40edaaa99eec80ff
marcelorang <mclowebdev@gmail.com>
2023-07-13 00:50:20 -0300

    api validations schema and install axios

--- components/modals/sotre-modal.tsx
+++ components/modals/sotre-modal.tsx
@@ -3,6 +3,7 @@
 import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod"
+import { useState } from "react";
 
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "@/components/ui/modal";
@@ -23,6 +24,8 @@ const formSchema = z.object({
 
 export const StoreModal = () => {
     const storeModal = useStoreModal();
+
+    const [loading, setLoading] = useState(false);
 
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -55,15 +58,20 @@ export const StoreModal = () => {
                   <FormItem>
                     <FormLabel>Name</FormLabel>
                     <FormControl>
-                    <Input placeholder="E-commerce" {...field}/>
+                    <Input disabled={loading} placeholder="E-commerce" {...field}/>
                     </FormControl>
                     <FormMessage/>
                   </FormItem>  
                 )} 
                 />
                 <div className="pt-6 space-x-2 flex items-center justify-end w-full">
-                <Button variant="outline" onClick={storeModal.onClose}>Cancel</Button>
-                <Button type="submit">Continue</Button>
+                <Button
+                 variant="outline"
+                 onClick={storeModal.onClose}
+                 disabled={loading}
+                 >Cancel
+                 </Button>
+                <Button disabled={loading} type="submit">Continue</Button>
                 </div>
                
              </form>
