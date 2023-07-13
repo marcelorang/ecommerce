2c924619417d735e0c76b3613bf886daa7116c3e
marcelorang <mclowebdev@gmail.com>
2023-07-13 01:13:35 -0300

    tratando erros do endpoint modal

--- components/modals/sotre-modal.tsx
+++ components/modals/sotre-modal.tsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useState } from "react";
+import { toast } from "react-hot-toast"
 
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "@/components/ui/modal";
@@ -38,11 +39,14 @@ export const StoreModal = () => {
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
       try{
         setLoading(true);
-        const response = await axios.post('/api/stores', values);
-        console.log(response.data)
-     
+          throw new Error("x");
+
+          const response = await axios.post('/api/stores', values);
+        
+          toast.success("Store created.");
+
       } catch (error) {
-        console.log(error);
+        toast.error("Something went wrong.");
      
       }finally{
         setLoading(false);
